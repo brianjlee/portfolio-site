0cea6d4da0def92e33bc8c7b13a37f95672e86ae
Brian Lee <brianjleeofcl@gmail.com>
2016-11-24 01:36:12 -0800

    Add form factor

--- Draft2/assets/js/index.js
+++ Draft2/assets/js/index.js
@@ -1,1 +1,26 @@
- $('.carousel.carousel-slider').carousel({full_width: true});
+(function() {
+  'use strict';
+
+  $('.carousel.carousel-slider').carousel({
+    full_width: true,
+    indicators: true
+  });
+
+  $('.scrollspy').scrollSpy({
+    scrollOffset: 0
+  });
+
+  $('.table-of-contents').pushpin({
+    top: $('main').offset().top,
+    bottom: $('main').offset().top + $('main').height() + $('footer').height(),
+    offset: 0
+  });
+
+  const carouselNext = function() {
+    $('.carousel.carousel-slider').carousel('next');
+  };
+
+  setInterval(carouselNext(), 1000);
+
+
+})();
